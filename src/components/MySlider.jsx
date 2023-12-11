388584abb2ff44b2af45bcc787524d5164306b47
deepakmaan525 <deepakmaan525@gmail.com>
2023-12-11 15:28:41 +0530

    updated

--- src/components/MySlider.jsx
+++ src/components/MySlider.jsx
@@ -5,11 +5,11 @@ function MySlider() {
 
   React.useEffect(() => {
     const typed = new Typed(el.current, {
-      strings: ['Vishnu', ' Prince','Deepak','Sanju','Vikram','Deepak Seth', 'Pankaj','Sameer','Ram Sharma','Shubham Tau','Manoj','Dinesh','Naman'],
+      strings: ['Vishnu', ' Prince', 'Deepak', 'Sanju', 'Vikram', 'Deepak Seth', 'Pankaj', 'Sameer', 'Ram Sharma', 'Shubham Tau', 'Manoj', 'Dinesh', 'Naman'],
       typeSpeed: 100,
       loop: true,
       startDelay: 100,
-        backSpeed: 100,
+      backSpeed: 100,
       backDelay: 300,
       smartBackspace: true,
       showCursor: false,
@@ -22,7 +22,7 @@ function MySlider() {
 
   return (
     <div className="App min-h-screen container mx-auto px-4 flex items-center">
-      <p className=' text-start text-2xl sm:text-6xl font-bold text-white'> Binod Developer's Team: <span className=' text-orange-400'  ref={el} />
+      <p className=' text-start text-2xl sm:text-6xl font-bold text-white'> Binod Developer's Team: <span className=' text-orange-400 ff-p' ref={el} />
       </p>
     </div>
   );
